f04dddd89e21e9dcf25ae9b7646c09b21bd9d3f5
AlphaHinex <AlphaHinex@gmail.com>
2017-04-07 12:10:49 +0800

    Fix bugs

--- ihos/deploy/entrypoint.sh
+++ ihos/deploy/entrypoint.sh
@@ -13,28 +13,42 @@ then
 fi
 
 # app-dev
-cd $WORKDIR/app-dev
-node proxy/proxy-server.js &
-cd $WORKDIR/app-dev/www
-python -m SimpleHTTPServer 9000 &
+if [ -d "$WORKDIR/app-dev" ];
+then
+    cd $WORKDIR/app-dev
+    node proxy/proxy-server.js &
+    cd $WORKDIR/app-dev/www
+    python -m SimpleHTTPServer 9000 &
+fi
 
 # app-master
-cd $WORKDIR/app-master
-sed -i "s/8080/8081/" proxy/proxy-server.js
-node proxy/proxy-server.js &
-cd $WORKDIR/app-master/www
-python -m SimpleHTTPServer 9001 &
+if [ -d "$WORKDIR/app-master" ];
+then
+    cd $WORKDIR/app-master
+    sed -i "s/8080/8081/" proxy/proxy-server.js
+    node proxy/proxy-server.js &
+    cd $WORKDIR/app-master/www
+    python -m SimpleHTTPServer 9001 &
+fi
 
 # admin
-cd $WORKDIR/admin
-sed -i "s/8080/8082/" proxy/proxy-server.js
-node proxy/proxy-server.js &
-cd $WORKDIR/admin/www
-python -m SimpleHTTPServer 9002 &
+if [ -d "$WORKDIR/admin" ];
+then
+    cd $WORKDIR/admin
+    sed -i "s/8080/8082/" proxy/proxy-server.js
+    node proxy/proxy-server.js &
+    cd $WORKDIR/admin/www
+    python -m SimpleHTTPServer 9002 &
+fi
 
 # merge
-cd $WORKDIR/merge
-sed -i "s/8080/8083/" proxy/proxy-server.js
-node proxy/proxy-server.js &
-cd $WORKDIR/merge/www
-python -m SimpleHTTPServer 9003 &
+if [ -d "$WORKDIR/merge" ];
+then
+    cd $WORKDIR/merge
+    sed -i "s/8080/8083/" proxy/proxy-server.js
+    node proxy/proxy-server.js &
+    cd $WORKDIR/merge/www
+    python -m SimpleHTTPServer 9003 &
+fi
+
+tail -f /dev/null
