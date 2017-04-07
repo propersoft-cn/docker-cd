d0e2173f52a5766269532703ae55bf9830d68e84
AlphaHinex <AlphaHinex@gmail.com>
2017-04-07 11:41:59 +0800

    Before test

--- ihos/deploy/entrypoint.sh
+++ ihos/deploy/entrypoint.sh
@@ -12,8 +12,29 @@ then
     git clone https://${GH_OAUTH_TOKEN}@github.com/propersoft-cn/ihos.git -b fe-deploy --depth=1 $WORKDIR
 fi
 
-cd $WORKDIR
+# app-dev
+cd $WORKDIR/app-dev
 node proxy/proxy-server.js &
+cd $WORKDIR/app-dev/www
+python -m SimpleHTTPServer 9000 &
 
-cd $WORKDIR/www
-python -m SimpleHTTPServer 9000
+# app-master
+cd $WORKDIR/app-master
+sed -i "s/8080/8081/" proxy/proxy-server.js
+node proxy/proxy-server.js &
+cd $WORKDIR/app-master/www
+python -m SimpleHTTPServer 9001 &
+
+# admin
+cd $WORKDIR/admin
+sed -i "s/8080/8082/" proxy/proxy-server.js
+node proxy/proxy-server.js &
+cd $WORKDIR/admin/www
+python -m SimpleHTTPServer 9002 &
+
+# merge
+cd $WORKDIR/merge
+sed -i "s/8080/8083/" proxy/proxy-server.js
+node proxy/proxy-server.js &
+cd $WORKDIR/merge/www
+python -m SimpleHTTPServer 9003 &
