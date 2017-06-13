e787766b004f67db7267397c4b527c43aeda6560
AlphaHinex <AlphaHinex@gmail.com>
2017-06-13 15:22:39 +0800

    Add HC part

--- icmp/entrypoint.sh
+++ icmp/entrypoint.sh
@@ -13,20 +13,20 @@ then
     git clone https://${GH_OAUTH_TOKEN}@github.com/propersoft-cn/icmp.git -b fe-deploy --depth=1 $WORKDIR
 fi
 
-# HC_HOME=/opt/hc
-# mkdir -p $HC_HOME
-# cd $HC_HOME
-# URL=https://cloud.propersoft.cn/teamcities/guestAuth/repository/downloadAll/icmp_Demo/lastSuccessful/artifacts.zip
-# echo 'Downloading from '$URL
-# curl -O $URL
-# if [ -e $HC_HOME/artifacts.zip ];
-# then
-#     echo 'Unziping artifacts.zip...'
-#     rm -rf $HC_HOME/www
-#     unzip -qq $HC_HOME/artifacts.zip -d $HC_HOME/www -x index.html *.apk
-#     rm -f $HC_HOME/artifacts.zip
-#     echo 'Done'
-# fi
+HC_HOME=/opt/hc
+mkdir -p $HC_HOME
+cd $HC_HOME
+URL=https://cloud.propersoft.cn/teamcities/guestAuth/repository/downloadAll/Icmp_Demo/lastSuccessful/artifacts.zip
+echo 'Downloading from '$URL
+curl -O $URL
+if [ -e $HC_HOME/artifacts.zip ];
+then
+    echo 'Unziping artifacts.zip...'
+    rm -rf $HC_HOME/www
+    unzip -qq $HC_HOME/artifacts.zip -d $HC_HOME/www -x index.html *.apk
+    rm -f $HC_HOME/artifacts.zip
+    echo 'Done'
+fi
 
 # app-dev
 if [ -d "$WORKDIR/app-dev" ];
